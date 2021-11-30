6d596766f87f50b50d0ad42c7897dec68752105b
forgiv3r <vuvkjqkvv@mozmail.com>
2021-11-30 09:50:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# dzgo
+# dzgo
+#aglo
+# webi
---
+++ components/home2/ServicesTwo.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="service_section_2 commonSection">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h4 class="sub_title">.</h4>
+          <h2 class="sec_title">.<br> .</h2>
+          <p class="sec_desc">
+            .<br>.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="icon_box_2 text-center">
+            <h3>.</h3>
+            <p>
+              .
+            </p>
+            <div class="iconWrap">
+              <i class="mei-settings"></i>
+            </div>
+            <nuxt-link to="/service-detail">discover more</nuxt-link>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="icon_box_2 text-center">
+            <h3>.</h3>
+            <p>
+              .
+            </p>
+            <div class="iconWrap">
+              <i class="mei-pie-chart"></i>
+            </div>
+            <nuxt-link to="/service-detail">discover more</nuxt-link>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-4">
+          <div class="icon_box_2 text-center">
+            <h3>.</h3>
+            <p>
+              .
+            </p>
+            <div class="iconWrap">
+              <i class="mei-transfer"></i>
+            </div>
+            <nuxt-link to="/service-detail">discover more</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "ServicesTwo"
+    }
+</script>
+
+<style scoped>
+
+</style>
